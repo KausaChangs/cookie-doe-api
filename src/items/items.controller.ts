42a50174f86a27cd042dad771da2f42e223ba92b
KausaChangs <changayakausa@gmail.com>
2022-09-24 19:51:29 +0200

    accessed paramas using id's

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { get } from 'http';
 import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
@@ -6,6 +15,11 @@ export class ItemsController {
   @Get()
   findAll(): string {
     return ' Get all items';
+  }
+
+  @Get(':id')
+  findOne(@Param() param): string {
+    return `Item ${param.id}`;
   }
 
   @Post()
